c1c9ebaf558e4f9a2b118c73a9e0d40d5609d70c
Ryan Faulhaber <faulhaber.14@osu.edu>
2017-07-10 08:59:22 -0400

    added tests, added travis

--- gflag_test.go
+++ gflag_test.go
@@ -40,6 +40,41 @@ func TestBool2(t *testing.T) {
 	}
 }
 
+func TestInt(t *testing.T) {
+	gargs.Args = []string{"testflag", "22"}
+	intValue := Int("", "testflag", "set when testing", 9)
+
+	_, ok := FlagMap["testflag"]
+
+	if !ok {
+		t.Error("Key was not added to flagmap")
+	}
+
+	Parse()
+
+	if *intValue != 22 || *intValue == 9 {
+		t.Error("reference was not set")
+	}
+}
+
+func TestInt2(t *testing.T) {
+	gargs.Args = []string{"testflag"}
+	gargs.ArgsMap["testflag"] = "22"
+	intValue := Int("", "testflag", "set when testing", 9)
+
+	_, ok := FlagMap["testflag"]
+
+	if !ok {
+		t.Error("Key was not added to flagmap")
+	}
+
+	Parse()
+
+	if *intValue != 22 || *intValue == 9 {
+		t.Error("reference was not set")
+	}
+}
+
 func TestMain(m *testing.M) {
 	// TODO: add Parse() here
 	retCode := m.Run()
